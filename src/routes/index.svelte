49730d80de401eb74abed5e5ad2f4316d5be29e0
Luke Edwards <luke.edwards05@gmail.com>
2021-05-17 11:31:32 -0700

    feat: attach svelte-kit template

---
+++ src/routes/index.svelte
@@ -0,0 +1,8 @@
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<style>
+	* {
+		color: green;
+	}
+</style>
---
+++ svelte.config.js
@@ -0,0 +1,18 @@
+import preprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-static';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	preprocess: preprocess(),
+
+	kit: {
+		adapter: adapter({
+			fallback: 'index.html'
+		}),
+		files: {
+			template: 'src/index.html'
+		}
+	}
+};
+
+export default config;
