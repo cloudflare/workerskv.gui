baf7b5fbd4fd9f0fde5fc9870aeb614e96e7e6c8
Luke Edwards <luke.edwards05@gmail.com>
2021-05-21 14:48:49 -0700

    fix: defer `events.ts` to avoid `build` error

--- src/lib/events.ts
+++ src/lib/events.ts
@@ -1,12 +1,18 @@
 import { WebviewWindow } from '@tauri-apps/api/window';
 
-addEventListener('keydown', async event => {
-	// Create a new window
-	if (event.metaKey && event.key === 'n') {
-		event.preventDefault();
-		// @ts-ignore - marked private
-		return new WebviewWindow(
-			Math.random().toString(36).slice(2)
-		);
-	}
-});
+if (typeof window !== 'undefined') {
+	addEventListener('keydown', async event => {
+		// Create a new window
+		if (event.metaKey && event.key === 'n') {
+			event.preventDefault();
+			// @ts-ignore - marked private
+			return new WebviewWindow(
+				Math.random().toString(36).slice(2)
+			);
+		}
+	});
+}
+
+// webview.once('tauri://error', function (e) {
+// 	console.log('an error happened creating the webview window');
+// });
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,7 +1,6 @@
 <script>
 	import '../index.css';
-	import { onMount } from 'svelte';
-	onMount(() => import('$lib/events'));
+	import('$lib/events');
 </script>
 
 <slot/>
