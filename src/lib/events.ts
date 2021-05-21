3747d48bbee6f0c6b9aa8c1f0d3913cfcd8fefa1
Luke Edwards <luke.edwards05@gmail.com>
2021-05-21 13:22:00 -0700

    chore: ui "cmd+n" fixes

--- src/lib/events.ts
+++ src/lib/events.ts
@@ -3,6 +3,7 @@ import { WebviewWindow } from '@tauri-apps/api/window';
 addEventListener('keydown', async event => {
 	// Create a new window
 	if (event.metaKey && event.key === 'n') {
+		event.preventDefault();
 		// @ts-ignore - marked private
 		return new WebviewWindow(
 			Math.random().toString(36).slice(2)
