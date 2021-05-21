a782bd95fa1cc27846d53f6e8637e5aa1d4ac906
Luke Edwards <luke.edwards05@gmail.com>
2021-05-20 21:42:06 -0700

    fix(color): keyboard navigation

--- src/lib/tags/Colors.svelte
+++ src/lib/tags/Colors.svelte
@@ -1,4 +1,4 @@
-<script>
+<script lang="ts">
 	let colors = [
 		'#fa5252', // red
 		'#e64980', // pink
@@ -17,13 +17,38 @@
 	export let id = '';
 	export let name = '';
 	export let value = colors[0];
+
+	function onFocus(ev: FocusEvent) {
+		let target = ev.target as HTMLElement;
+		let active = target.querySelector('.active') as HTMLElement;
+		if (active) active.focus();
+	}
+
+	function onArrows(ev: KeyboardEvent) {
+		let key = ev.which;
+		let tmp: Nullable<HTMLElement>;
+		let target = ev.target as HTMLElement;
+
+		if (key === 39) {
+			// RIGHT ARROW
+			ev.preventDefault();
+			tmp = target.nextElementSibling as HTMLElement;
+			if (tmp) tmp.click(),tmp.focus();
+		} else if (key === 37) {
+			// LEFT ARROW
+			ev.preventDefault();
+			tmp = target.previousElementSibling as HTMLElement;
+			if (tmp) tmp.click(),tmp.focus();
+		}
+	}
 </script>
 
 <input {id} {name} type="hidden" bind:value >
 
-<div style="--n: {colors.length}">
+<div tabindex="0" style="--n: {colors.length}" on:keydown={onArrows} on:focus={onFocus}>
 	{#each colors as color (color)}
 	<span
+		tabindex="-1"
 		class:active={color === value}
 		on:click={() => value=color}
 		style="--c: {color}"
@@ -36,6 +61,7 @@
 		display: grid;
 		grid-template-columns: repeat(var(--n), 1fr);
 		column-gap: 4px;
+		outline: none;
 	}
 
 	span {
