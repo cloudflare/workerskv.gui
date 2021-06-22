922d79d2282032748c30df1f7f6eedbfb28fed6a
Luke Edwards <luke.edwards05@gmail.com>
2021-06-21 17:17:45 -0700

    chore: fix misc typos

--- src-tauri/src/menu.rs
+++ src-tauri/src/menu.rs
@@ -52,15 +52,52 @@ pub fn mainmenu() -> Vec<Menu<String>> {
 		]
 	};
 
-	// Attention, Windows only support custom menu for now.
+	// Attention:
+	// Windows only support custom menu for now.
 	// If we add any `MenuItem::*` they'll not render
 	// We need to use custom menu with `Menu::new()` and catch
 	// the events in the EventLoop.
 	#[cfg(target_os = "windows")]
-	let menu = vec![
-		Menu::new("File", vec![other_test_menu]),
-		Menu::new("Other menu", vec![quit_menu]),
-	];
+	let menu = {
+		vec![
+			// Menu::new("Workers KV", vec![
+			// 	MenuItem::About(
+			// 		"Workers KV".to_string()
+			// 	),
+			// 	MenuItem::Services,
+			// 	MenuItem::Separator,
+			// 	MenuItem::Hide,
+			// 	MenuItem::HideOthers,
+			// 	MenuItem::ShowAll,
+			// 	MenuItem::Separator,
+			// 	MenuItem::Quit,
+			// ]),
+			Menu::new("File", vec![
+				new_window,
+				// MenuItem::Separator,
+				// MenuItem::CloseWindow,
+			]),
+			// Menu::new("Edit", vec![
+			// 	MenuItem::Undo,
+			// 	MenuItem::Redo,
+			// 	MenuItem::Separator,
+			// 	MenuItem::Cut,
+			// 	MenuItem::Copy,
+			// 	MenuItem::Paste,
+			// 	MenuItem::SelectAll,
+			// ]),
+			// Menu::new("View", vec![
+			// 	MenuItem::EnterFullScreen
+			// ]),
+			// Menu::new("Window", vec![
+			// 	MenuItem::Minimize,
+			// 	MenuItem::Zoom,
+			// ]),
+			Menu::new("Help", vec![
+				new_issue
+			]),
+		]
+	};
 
 	menu
 }
